1a2fcd87ce5f73634ee5e8ab788376662fa2cd78
junlun <junlun.fan@gmail.com>
2014-09-11 22:04:27 +0800

    第二次重温TTD，需要认真了，争取在两晚把之前的东西都捡起来

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,35 @@
+# -*- coding:utf-8 -*-
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitortest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        #用户打开网站，localhost:8000
+        self.browser.get('http://localhost:8000')
+        #用户发现网站的title以及header上面写着To-Do
+        self.assertIn('To-Do',self.browser.title)
+        self.fail('Finish the test')
+        #用户被要求在文本框中输入一个to-do项目
+
+        #用户输入"buy peacock feathers"
+
+        #用户输入enter后，页面跳转，页面显示"1: buy peacock feathers"
+        #这里仍然有个文本框用于第二个to-do项目的输入，用户继续输入"use peacock feathers to make a fly"
+
+        #用户输入enter后页面跳转，页面显示两个之前用户输入的to-do项目
+
+        #用户需要单独的list url
+
+        #用户输入独立的url，能够看到自己之前的to-do项目list
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
