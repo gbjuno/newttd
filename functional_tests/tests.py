f4a429257ef9da54991eb7a6059d9bad28e5b020
junlun <junlun.fan@gmail.com>
2014-09-12 16:29:29 +0800

    functional_tests新增新的网页功能

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -35,7 +35,8 @@ class NewVisitortest(LiveServerTestCase):
 
         #用户输入enter后，页面跳转，页面显示"1: buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
-        
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url,'/lists/.+')
         self.check_for_row_in_list_table('1: buy peacock feathers')
         #这里仍然有个文本框用于第二个to-do项目的输入，用户继续输入"use peacock feathers to make a fly"
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -45,5 +46,23 @@ class NewVisitortest(LiveServerTestCase):
         self.check_for_row_in_list_table('1: buy peacock feathers')
         self.check_for_row_in_list_table('2: use peacock feathers to make a fly')
         #用户需要单独的list url
-        self.fail('finish the test')
         #用户输入独立的url，能够看到自己之前的to-do项目list
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('buy peacock feathers', page_text)
+        self.assertIn('buy milk', page_text)
